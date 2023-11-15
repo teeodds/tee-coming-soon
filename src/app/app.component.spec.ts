b946f09f7000ec2db6c3d054353243cc99927436
TEE\nutta <tee@odds.team>
2023-11-15 14:52:23 +0700

    feat: rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wts-coming-soon'`, () => {
+  it(`should have as title 'tee-coming-soon'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('wts-coming-soon');
+    expect(app.title).toEqual('tee-coming-soon');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wts-coming-soon app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('tee-coming-soon app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wts-coming-soon';
+  title = 'tee-coming-soon';
 }
